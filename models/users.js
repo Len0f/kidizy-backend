d539b343b4ccc092da948e716b73ddbb488af2bd
ErgoMundusDovi <eddylou91@gmail.com>
2025-08-04 12:42:05 +0200

    small update

--- models/users.js
+++ models/users.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
+const parentInfosShema =mongoose.Schema({
+    kids: Number,
+    subscription: Boolean
+})
+
+const babysitterInfosShema =mongoose.Schema({
+    CNI: String,
+    criminalRecord: String,
+    age: String,
+    price: Number,
+    bio: String,
+    availability: Date,
+    interest: String,
+    isDocOk: Boolean,
+    situation: String,
+    jackpot: Number
+})
 
 const usersSchema = mongoose.Schema({
 	token: String,
@@ -20,23 +37,6 @@ const usersSchema = mongoose.Schema({
     babysitterInfos: babysitterInfosShema,
 });
 
-const parentInfosShema =mongoose.Schema({
-    kids: Number,
-    subscription: Boolean
-})
-
-const babysitterInfosShema =mongoose.Schema({
-    CNI: String,
-    criminalRecord: String,
-    age: String,
-    price: Number,
-    bio: String,
-    availability: Date,
-    interest: String,
-    isDocOk: Boolean,
-    situation: String,
-    jackpot: Number
-})
 
 const Users = mongoose.model('users', usersSchema);
 
--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,31 @@ const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['username', 'password'])) {
+  if (!checkBody(req.body, ['email', 'password'])) {
     res.json({ result: false, error: 'Champs manquants ou vides' });
     return;
+  } else if (!EMAIL_REGEX.test(req.body.email)){
+    res.json({Error:`${req.body.email} n'est pas une adresse valide`})
+    return
   }
 
 
   // Vérifiez si l'utilisateur n'a pas déjà été enregistré
 
 
-  User.findOne({ username: req.body.username }).then(data => {
+  User.findOne({ email: req.body.email }).then(data => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
 
       const newUser = new User({
-        username: req.body.username,
+        email: req.body.email,
         password: hash,
         token: uid2(32),
-        canBookmark: true,
+        
       });
 
 
@@ -45,13 +49,13 @@ router.post('/signup', (req, res) => {
 
 
 router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['username', 'password'])) {
+  if (!checkBody(req.body, ['email', 'password'])) {
     res.json({ result: false, error: 'Champs manquants ou vides' });
     return;
   }
 
 
-  User.findOne({ username: req.body.username }).then(data => {
+  User.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
