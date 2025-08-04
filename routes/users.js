d2c09fa3af20dab553b23f9ba7970066294752a4
cedricbarthelot <cedric.barthelot@outlook.fr>
2025-08-04 11:48:19 +0200

    routes signin signup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,75 @@
 var express = require('express');
 var router = express.Router();
 
+
+require('../connection/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Champs manquants ou vides' });
+    return;
+  }
+
+
+  // Vérifiez si l'utilisateur n'a pas déjà été enregistré
+
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+
+      const newUser = new User({
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+        canBookmark: true,
+      });
+
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      // L'utilisateur existe déjà dans la base de données
+
+
+      res.json({ result: false, error: 'L’utilisateur existe déja' });
+    }
+  });
+});
+
+
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Champs manquants ou vides' });
+    return;
+  }
+
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'Utilisateur introuvable ou mot de passe incorrect' });
+    }
+  });
+});
+
+
+
+module.exports = router 
+
+
+
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
