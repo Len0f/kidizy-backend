2018bdca26e02806f3eb260d85d2c2549e11c1f2
cedricbarthelot <cedric.barthelot@outlook.fr>
2025-08-06 17:57:26 +0200

    screendispo

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use('/messages',messageRouter)
-
 app.use('/gardes', gardesRouter);
 
 
