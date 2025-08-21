510175b35b42d7ddd49c815174b095e25d489f9f
Your nameLen0f <caroline.viot90@gmail.com>
2025-08-21 21:17:35 +0200

    Correction

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config(); // Charge les variables d'environnement depuis .env
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var gardesRouter = require('./routes/gardes');
-var conversationRouter = require ('./routes/conversations')
-var messageRouter = require ('./routes/messages')
-
-
-var propositionRouter = require('./routes/propositions')
+// Importation des routeurs
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var gardesRouter = require("./routes/gardes");
+var conversationRouter = require("./routes/conversations");
+var messageRouter = require("./routes/messages");
+var propositionRouter = require("./routes/propositions");
 
 var app = express();
 
-const cors = require('cors');
+// ------------------- MIDDLEWARES GLOBAUX
+const cors = require("cors");
 app.use(cors());
-const fileUpload = require('express-fileupload');
-app.use(fileUpload({
-  useTempFiles: true,           
-  tempFileDir: './tmp',     
-  createParentPath: true,       
-}));
-app.use(logger('dev'));
+const fileUpload = require("express-fileupload");
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./tmp",
+    createParentPath: true,
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/propositions',propositionRouter)
-app.use('/conversations',conversationRouter)
+// ------------------- ROUTES
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/propositions", propositionRouter);
+app.use("/conversations", conversationRouter);
 
-app.use('/messages',messageRouter)
-app.use('/gardes', gardesRouter);
-
-app.use(require('express-fileupload')());
+app.use("/messages", messageRouter);
+app.use("/gardes", gardesRouter);
 
 module.exports = app;
